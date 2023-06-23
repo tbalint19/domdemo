707b63f0668f56cc3defabd4ed85c3d6c609b8d1
tbalint19 <tothbalint719@gmail.com>
2023-06-23 19:52:05 +0200

    fetch

--- script.ts
+++ script.ts
@@ -1,35 +1,16 @@
-let myEventListener = (event: Event) => {
-  let userInput = (event.target as HTMLInputElement).value
-  let result = userInput.split("").reverse().join("")
-  renderH1(result)
-  renderP(userInput)
-}
-
-// input
-let resultHeaderElement = document.getElementById("result-header")!
 let resultParagraphElement = document.getElementById("result-paragraph")!
-let inputElement = document.getElementById("my-input")!
-let buttonElement = document.getElementById("my-button")!
 
-inputElement.addEventListener("input", myEventListener)
-buttonElement.addEventListener("click", myEventListener)
-
-
-// output
-let renderH1 = (content: string) => {
-  if (content.length > 5) {
-    resultHeaderElement.innerHTML = "<h1>" + content + "</h1>" // !!!!!
-  } else {
-    resultHeaderElement.innerHTML = ""
-  }
+let render = (content: string) => {
+  resultParagraphElement.innerHTML = content
 }
 
-let renderP = (content: string) => {
-  let toRender = ""
-  let count = 0
-  while (count < 150) {
-    toRender += "<p class='my-class'>" + content + "</p>"
-    count++
-  }
-  resultParagraphElement.innerHTML = toRender
+const load = async () => {
+  let response = await fetch("https://rickandmortyapi.com/api/character")
+  let data = await response.json()
+
+  
+  render(data["info"]["pages"])
 }
+
+let loadButtonElement = document.getElementById("load")!
+loadButtonElement.addEventListener("click", load)
